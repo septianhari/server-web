61f39a943366ffb1a4bd02ee8a4705baa2ccadcc
sabarNo <aribarno86@gmail.com>
2024-12-11 03:39:48 +0000

    push

--- src/server/server.js
+++ src/server/server.js
@@ -1,53 +1,49 @@
 require('dotenv').config();
-
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
-
+ 
 (async () => {
     const server = Hapi.server({
-        port: process.env.PORT ||4040,
+        port: process.env.PORT || 4040,
         host: '0.0.0.0',
         routes: {
             cors: {
-                origin: ['*'],
+              origin: ['*'],
             },
         },
     });
-
+ 
     const model = await loadModel();
     server.app.model = model;
-
+ 
     server.route(routes);
-
+ 
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
-
-        if (response.isBoom && response.output.statusCode === 413) {
+ 
+        if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
-                message: 'Payload content length greater than maximum allowed: 1000000',
-            });
-                        
-            newResponse.code(413);
+                message: `${response.message}`
+            })
+            newResponse.code(response.statusCode)
             return newResponse;
         }
-
-        if (response instanceof InputError || response.isBoom) {
-            const statusCode = response instanceof InputError ? response.statusCode : response.output.statusCode;
+ 
+        if (response.isBoom) {
             const newResponse = h.response({
                 status: 'fail',
-                message: 'Terjadi kesalahan dalam melakukan prediksi',
-            });
-
-            newResponse.code(parseInt(statusCode));
+                message: response.message
+            })
+            newResponse.code(response.output.statusCode)
             return newResponse;
         }
-
+ 
         return h.continue;
     });
-
+ 
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
